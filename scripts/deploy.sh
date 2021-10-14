463d9f366b3a02b49ff058492dcf1b0f0f7483bf
Candice Canoso <mail@cndce.me>
2021-10-14 09:27:33 +0800

    Fixed deploy script; Ignore all local changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,12 @@
 eval $(egrep -v '^#' .env | xargs)
 
-ssh $SSH_HOST "cd $SSH_PATH; git fetch; git checkout $GIT_DEPLOY_BRANCH; pm2 delete $PM2_NAME; npm install; npm run build; pm2 start npx --name \"$PM2_NAME\" -- next start -p $DEPLOY_PATH; pm2 logs $PM2_NAME"
+ssh $SSH_HOST "cd $SSH_PATH; 
+
+git fetch; 
+git reset --hard $GIT_DEPLOY_BRANCH; 
+
+pm2 delete $PM2_NAME; 
+npm install; 
+npm run build; 
+pm2 start npx --name \"$PM2_NAME\" -- next start -p $DEPLOY_PATH; 
+pm2 logs $PM2_NAME"
