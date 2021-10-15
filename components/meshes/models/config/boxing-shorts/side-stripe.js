bf6a0c0b8ff20d30a7a966ec94a25b43bd6d17f2
Candice <kaye_candy14@yahoo.com>
2021-10-15 19:22:39 +0800

    Added side-stripe boxing shorts

---
+++ components/meshes/models/config/boxing-shorts/side-stripe.js
@@ -0,0 +1,113 @@
+import { tasselMaterial } from "./_tasselMaterial";
+
+const partsSideSlit = [
+  {
+    id: "stripe",
+    name: "Stripe Material",
+    url: "/3d/boxing-shorts/side-stripe/stripe_side-slit.glb",
+    hasTexture: true,
+  },
+  {
+    id: "stitching-bottom",
+    name: "Leg Trim",
+    url: "/3d/boxing-shorts/side-stripe/stitching-bottom_side-slit.glb",
+    hasTexture: true,
+  },
+];
+
+const partsCurve = [
+  {
+    id: "stripe",
+    name: "Stripe Material",
+    url: "/3d/boxing-shorts/side-stripe/stripe_curve.glb",
+    hasTexture: true,
+  },
+  {
+    id: "stitching-bottom",
+    name: "Leg Trim",
+    url: "/3d/boxing-shorts/side-stripe/stitching-bottom_curve.glb",
+    hasTexture: true,
+  },
+];
+
+const transformRetro = (node) => {
+  if (node.id != "waist" && node.id != "waist-logo") {
+    return {
+      scale: [1, 0.8, 1],
+    };
+  }
+
+  return {
+    position: [0, -0.252, 0],
+  };
+};
+
+const CONTENT = {
+  preview: "/3d/boxing-shorts/side-stripe/preview.webp",
+  models: [
+    {
+      id: "waist",
+      name: "Waistband",
+      url: "/3d/boxing-shorts/waistband/waist.glb",
+      hasTexture: true,
+    },
+    {
+      id: "waist-logo",
+      name: "Waistband Logo",
+      url: "/3d/boxing-shorts/waistband/waist-logo.glb",
+      hasTexture: true,
+    },
+    {
+      id: "body",
+      name: "Main Colour",
+      url: "/3d/boxing-shorts/side-stripe/body.glb",
+      hasTexture: true,
+    },
+    {
+      id: "stitching",
+      name: "Vertical Trim",
+      url: "/3d/boxing-shorts/side-stripe/stitching.glb",
+      hasTexture: true,
+    },
+  ],
+  variations: [
+    {
+      legCut: "side-slit",
+      size: "standard",
+      parts: partsSideSlit,
+    },
+    {
+      legCut: "curve",
+      size: "standard",
+      parts: partsCurve,
+    },
+    {
+      legCut: "side-slit",
+      size: "retro",
+      parts: partsSideSlit,
+      transforms: transformRetro,
+    },
+    {
+      legCut: "curve",
+      size: "retro",
+      parts: partsCurve,
+      transforms: transformRetro,
+    },
+    {
+      hasTassels: true,
+      parts: [
+        {
+          id: "tassels-side",
+          name: "Tassels",
+          url: "/3d/boxing-shorts/side-stripe/tassels.glb",
+          material: tasselMaterial,
+          meshProps: {
+            renderOrder: 1,
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export default CONTENT;
