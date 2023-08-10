131ee74937cca80db41c50e71e4d777481fe0c32
Candice Canoso <kaye_candy14@yahoo.com>
2023-08-10 18:28:26 +0800

    Refactor import url

--- src/components/controls/controls-model/_config.js
+++ src/components/controls/controls-model/_config.js
@@ -1,8 +1,8 @@
-import { default as side_stripe } from "/components/meshes/models/config/boxing-shorts/side-stripe";
-import { default as classic } from "/components/meshes/models/config/boxing-shorts/classic";
-import { default as slick } from "/components/meshes/models/config/boxing-shorts/slick";
-import { default as classic_robe } from "/components/meshes/models/config/robes/classic";
-import { default as playboy } from "/components/meshes/models/config/robes/playboy";
+import { default as side_stripe } from "/src/components/meshes/models/config/boxing-shorts/side-stripe";
+import { default as classic } from "/src/components/meshes/models/config/boxing-shorts/classic";
+import { default as slick } from "/src/components/meshes/models/config/boxing-shorts/slick";
+import { default as classic_robe } from "/src/components/meshes/models/config/robes/classic";
+import { default as playboy } from "/src/components/meshes/models/config/robes/playboy";
 
 import BoxingShortsModel from "../../meshes/models/BoxingShortsModel";
 import RobesModel from "../../meshes/models/RobesModel";
